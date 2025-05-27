58809f3a49cb0a124e77ec295653d4c57b93fd8e
KennyWarn <kfwarner@eiu.edu>
2025-05-26 22:48:52 -0500

    Updates file structure

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,9 +8,9 @@
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="/" class="<?= urlIs("/") ? "bg-gray-900 text-white" : "text-gray-300"; ?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Home</a>
-                        <a href="/controllers/about.php" class="<?= urlIs("/about.php") ? "bg-gray-900 text-white" : "text-gray-300";?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                        <a href="/controllers/contact.php" class="<?= urlIs("/contact.php") ? "bg-gray-900 text-white" : "text-gray-300";?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                        <a href="/MyWebsite/" class="<?= urlIs("/MyWebsite/") ? "bg-gray-900 text-white" : "text-gray-300"; ?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Home</a>
+                        <a href="/MyWebsite/about" class="<?= urlIs("/MyWebsite/about") ? "bg-gray-900 text-white" : "text-gray-300";?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
+                        <a href="/MyWebsite/contact" class="<?= urlIs("/MyWebsite/contact") ? "bg-gray-900 text-white" : "text-gray-300";?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
                     </div>
                 </div>
             </div>
