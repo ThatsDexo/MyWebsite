faa6d416fbd98496aa2aedcfef1416b6e428b17b
KennyWarn <kfwarner@eiu.edu>
2025-05-26 22:49:56 -0500

    Added router functionality

---
+++ router.php
@@ -0,0 +1,25 @@
+<?php
+
+$uri = parse_url($_SERVER["REQUEST_URI"])["path"];
+
+
+$routes = [
+"/MyWebsite/" => "controllers/index.php",
+"/MyWebsite/about" => "controllers/about.php",
+"/MyWebsite/contact" => "controllers/contact.php"
+];
+
+function abort($code = 404){
+    http_response_code($code);
+    require "views/{$code}.php";
+}
+
+function routeToController($uri, $routes){
+    if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+    } else {
+    abort();
+    }
+}
+
+routeToController($uri, $routes);
---
+++ views/404.php
@@ -0,0 +1,9 @@
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+     <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1>Error 404.</h1>
+            <p>Sorry, an error occurred.</p>
+        </div>
+    </main>
+<?php require('partials/footer.php') ?>
